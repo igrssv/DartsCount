347a8ac9f014864b2b5ef6c6546ec978ee48609e
Igor Sysoev <igro.sia@yandex.ru>
2023-10-03 16:53:59 +0300

    Initial Commit

---
+++ DartsCount/DartsCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartsCountApp.swift
+//  DartsCount
+//
+//  Created by Игорь Сысоев on 03.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct DartsCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
